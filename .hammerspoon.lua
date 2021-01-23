f7490bbda580d5628cf2af7ba6c5c9d7a32e9ec7
Martin Rue <hello@martinrue.com>
2021-01-23 02:28:55 +0000

    Add Telegram and Postico to auto positioning

--- .hammerspoon.lua
+++ .hammerspoon.lua
@@ -2,14 +2,19 @@ hs.window.animationDuration = 0
 
 hs.hotkey.bind({"cmd", "alt"}, "A", function()
   local screen = "Color LCD"
-  local rect = hs.geometry.rect(0.015, 0.025, 0.97, 0.95)
+
+  local center1 = hs.geometry.rect(0.015, 0.025, 0.97, 0.95)
+  local center2 = hs.geometry.rect(0.05, 0.10, 0.90, 0.80)
+  local center3 = hs.geometry.rect(0.125, 0.20, 0.75, 0.60)
   
   hs.layout.apply({
-    {"Code", nil, screen, rect, nil, nil},
-    {"iTerm2", nil, screen, rect, nil, nil},
-    {"Xcode", nil, screen, rect, nil, nil},
-    {"Safari", nil, screen, rect, nil, nil},
-    {"Firefox", nil, screen, rect, nil, nil},
-    {"Google Chrome", nil, screen, rect, nil, nil},
+    {"Code", nil, screen, center1, nil, nil},
+    {"iTerm2", nil, screen, center1, nil, nil},
+    {"Xcode", nil, screen, center1, nil, nil},
+    {"Postico", nil, screen, center2, nil, nil},
+    {"Safari", nil, screen, center1, nil, nil},
+    {"Firefox", nil, screen, center1, nil, nil},
+    {"Google Chrome", nil, screen, center1, nil, nil},
+    {"Telegram", nil, screen, center3, nil, nil},
   })
 end)
