691d3dd0acbb0f4b1e4ac2850f77a91f67b2b3ed
Martin Rue <hello@martinrue.com>
2024-08-22 13:06:05 +0700

    Update auto formatting options to allow disabling

--- nvim/lua/martinrue/plugins/formatting.lua
+++ nvim/lua/martinrue/plugins/formatting.lua
@@ -18,11 +18,31 @@ return {
 				markdown = { "prettier" },
 				lua = { "stylua" },
 			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
+			format_on_save = function(bufnr)
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+
+				return { timeout_ms = 500, lsp_format = "fallback" }
+			end,
+		})
+
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
