4a353185f7eaf7901d9664341b1d04225adc7188
Martin Rue <hello@martinrue.com>
2024-12-16 12:08:37 +0700

    Fix issue in Hammerspoon config

--- .hammerspoon.lua
+++ .hammerspoon.lua
@@ -1,17 +1,14 @@
 hs.window.animationDuration = 0
 
 hs.hotkey.bind({ "cmd", "alt" }, "A", function()
-	local screen = "Color LCD"
+	local screen = hs.screen.mainScreen()
 
 	local center1 = hs.geometry.rect(0.008, 0.025, 0.985, 0.95)
 	local center2 = hs.geometry.rect(0.05, 0.10, 0.90, 0.80)
 	local center3 = hs.geometry.rect(0.125, 0.20, 0.75, 0.60)
 
 	hs.layout.apply({
-		{ "Code", nil, screen, center1, nil, nil },
-		{ "iTerm2", nil, screen, center1, nil, nil },
 		{ "WezTerm", nil, screen, center1, nil, nil },
-		{ "Xcode", nil, screen, center1, nil, nil },
 		{ "Postico", nil, screen, center2, nil, nil },
 		{ "Safari", nil, screen, center1, nil, nil },
 		{ "Firefox", nil, screen, center1, nil, nil },
