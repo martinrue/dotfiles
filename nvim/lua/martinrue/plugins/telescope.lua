1bac0662f9d29b3c37d1c51d8e3609bce7b8f601
Martin Rue <hello@martinrue.com>
2025-04-20 17:34:06 +0700

    Update Telescope config to exclude vendor and not show hidden files/dirs

--- nvim/lua/martinrue/plugins/telescope.lua
+++ nvim/lua/martinrue/plugins/telescope.lua
@@ -16,6 +16,7 @@ return {
 				path_display = { "smart" },
 				file_ignore_patterns = {
 					"node_modules",
+					"vendor",
 					".DS_Store",
 				},
 				mappings = {
@@ -28,12 +29,7 @@ return {
 
 		telescope.load_extension("fzf")
 
-		vim.keymap.set(
-			"n",
-			"<leader>ff",
-			"<cmd>Telescope find_files hidden=true<CR>",
-			{ desc = "Fuzzy find files in cwd" }
-		)
+		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
 		vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
 		vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" })
 		vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Fuzzy find files in cwd" })
