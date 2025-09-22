209f1205de73448cbe420193407d7eb0053eb193
Martin Rue <hello@martinrue.com>
2025-09-22 09:37:01 +0100

    Add back cmd to show inline diagnostics

--- nvim/lua/martinrue/plugins/lsp/lspconfig.lua
+++ nvim/lua/martinrue/plugins/lsp/lspconfig.lua
@@ -3,6 +3,16 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			vim.lsp.enable({ "ts_ls", "html", "cssls", "gopls", "eslint" })
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					local opts = { buffer = ev.buf, silent = true }
+
+					opts.desc = "Show line diagnostics"
+					vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+				end,
+			})
 		end,
 	},
 }
